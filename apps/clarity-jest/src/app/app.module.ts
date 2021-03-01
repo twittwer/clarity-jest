4f31e43508428377c0d7dd761c3214bec45f9702
Tobias Wittwer <t.wittwer95@gmail.com>
2021-03-01 18:19:50 +0100

    add clarity

--- apps/clarity-jest/src/app/app.module.ts
+++ apps/clarity-jest/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule],
   declarations: [AppComponent],
-  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
