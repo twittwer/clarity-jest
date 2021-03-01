efc8bab39c95a664f093abcb9987c2a88e5304f1
Tobias Wittwer <t.wittwer95@gmail.com>
2021-03-01 18:26:43 +0100

    setup sample in app component

--- apps/clarity-jest/src/app/app.component.spec.ts
+++ apps/clarity-jest/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ClarityModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -12,20 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'clarity-jest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('clarity-jest');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to clarity-jest!'
-    );
   });
 });
